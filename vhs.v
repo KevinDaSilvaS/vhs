b90422cff9ce1b28cb754a406663a1d6ee7e8c4b
Your Name <kevin.kds80@gmail.com>
2022-10-28 21:20:46 -0300

    add new module structure

--- vhs.v
+++ vhs.v
@@ -1,44 +1,5 @@
 module vhs
 
-pub fn take<T>(amount int, ls []T) []T {
-	arr_size := ls.len
-	if amount >= arr_size {
-		return ls 
-	}
-	
-	return []T{len: amount, init: ls[0 + it]}
-}
-
-pub fn drop<T>(amount int, ls []T) []T {
-	arr_size := ls.len
-	if amount >= arr_size-1 {
-		return []
-	}
-	
-	return []T{len: arr_size-amount, init: ls[it+amount]}
-}
-
-pub fn head<T>(ls []T) T {
-	return ls[0]
-}
-
-pub fn last<T>(ls []T) T {
-	return ls[ls.len-1]
-}
-
-pub fn init<T>(ls []T) []T {
-	arr_size := ls.len
-	if arr_size <= 1 {
-		return []
-	}
-	
-	return take(arr_size-1, ls)
-}
-
-pub fn tail<T>(ls []T) []T {
-	return drop(1, ls)
-}
-
 fn main() {
 	println('hi')
 }
